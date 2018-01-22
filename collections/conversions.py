dd9c81f486d7cb15eeba28b8cb5848f918239db5
saurabhse <saurabh10seth@gmail.com>
2018-01-22 14:26:14 +0530

    Update conversions.py

--- collections/conversions.py
+++ collections/conversions.py
@@ -10,5 +10,5 @@ str_var = ",".join(list_var)
 
 """ add string variable to all elements of list """
 list_var = ['a','b','c']
-new_list_var = map(lambda dyn_col: dyn_col + " dynamic_string", list_var)
+new_list_var = map(lambda list_elem: list_elem + " dynamic_string", list_var)
 print(','.join(new_list_var)) # a dynamic_string,b dynamic_string,c dynamic_string
