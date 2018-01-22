9ef5c3a182dc09fa55cdf29364ae85f657f0b902
saurabhse <saurabh10seth@gmail.com>
2018-01-22 14:16:25 +0530

    Update read_xml_file.py

--- filehandling/read_xml_file.py
+++ filehandling/read_xml_file.py
@@ -1,16 +1,22 @@
 
 """
 This will read xml using minidom and get the value of node by its name
-<root>
+xml_text = <root>
 	<node>
-		<name>Hello</name>
+		<name id="key">Hello</name>
 	</node>
 </root>
+OR
+above contents in /dir_of_file/file_name
 """
 import xml.dom.minidom as minidom
 import os
 
 loader_config = minidom.parse(os.path.join(dir_of_file,file_name))
+# to load xml string
+# loader_config = mindom.parseString(xml_text)
 node = loader_config.getElementsByTagName("node")
 elem = node.getElementsByTagName("name")
-print(elem[0].firstChild.data)
+print(elem[0].firstChild.data) # fetches node value - Hello
+node = loader_config.getElementsByTagName("name")
+print(elem.attributes['id'].value) # fetches node attribute - key
