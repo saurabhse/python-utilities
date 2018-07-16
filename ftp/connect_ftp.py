46bd8703d717408dcc24ea36a92212192556aac6
saurabhse <saurabh10seth@gmail.com>
2018-07-16 20:22:24 +0530

    connect ftp site

---
+++ ftp/connect_ftp.py
@@ -0,0 +1,17 @@
+import paramiko
+
+def connect():
+  key = paramiko.RSAKey.from_private_key_file(host_key)
+  user = ""
+  password=""
+  proxy_comm = "ssh -o UserKnownHostsFile= -o StrictHostKeyChecking=no -i /dsa_key -l user_name sftpproxy nc <ftp_loc_host_name> <port>"
+  proxy = paramiko.ProxyCommand(proxy_command)
+  transport = paramiko.Transport(proxy)
+  transport.connect()
+  transport.set_hexdump(True)
+  transport.auth_publickey(username=user,key=key)
+  transport.auth_password(username=user, password=password)
+  return paramiko.SFTPClient.from_transport(transport)
+  
+ if __name__ == "main":
+  
