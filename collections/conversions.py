d73bd0309c8a84321a99a05346437ed0fca3a3d3
saurabhse <saurabh10seth@gmail.com>
2018-07-23 13:49:18 +0530

    Update conversions.py

--- collections/conversions.py
+++ collections/conversions.py
@@ -12,3 +12,7 @@ str_var = ",".join(list_var)
 list_var = ['a','b','c']
 new_list_var = map(lambda list_elem: list_elem + " dynamic_string", list_var)
 print(','.join(new_list_var)) # a dynamic_string,b dynamic_string,c dynamic_string
+
+
+""" cast data to type and convert to list"""
+ids = list(map(int,ids))
