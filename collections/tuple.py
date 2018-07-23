3be5cd5a9f18e697266d2d0ad7d595a2572a9948
saurabhse <saurabh10seth@gmail.com>
2018-07-23 13:40:52 +0530

    Create tuple.py

---
+++ collections/tuple.py
@@ -0,0 +1,11 @@
+  # Named tuple
+  
+  def fetch_tuple():
+    Named_tuple = namedtuple('MyTuple','param1 param2')
+    return Named_tuple(param1='a',param2= 'b')
+    
+  if __name__ == "main"
+    tuple = fetch_tuple()
+    print(tuple.param1)
+    print(tuple.param2
+  
